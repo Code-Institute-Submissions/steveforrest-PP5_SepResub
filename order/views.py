3050f6b199050ab656d48df0e1354c0348cd87b7
steveforrest <steveforrest_1@hotmail.com>
2022-05-16 18:26:59 +0000

    corrected error in remove view

--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 from products.models import Product
 
@@ -55,7 +55,7 @@ def remove_from_order(request, item_id):
         order = request.session.get('order', {})
         
         if item_id in list(order.keys()):
-            order[item_id] = quantity
+            order[item_id] = 0
             messages.info(request, f'adjusted {product.name} on your {order[item_id]}')
         else:
             order.pop(item_id)
@@ -63,5 +63,5 @@ def remove_from_order(request, item_id):
             
         request.session['order'] = order
         return HttpResponse(status=200)
-    except exception as e:
-        return HTTpResponse(status=500)
+    except Exception as e:
+        return HttpResponse(status=500)
