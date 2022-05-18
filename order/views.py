8ebdf2e76f3081bf021451b5615067d7f13d34f5
steveforrest <steveforrest_1@hotmail.com>
2022-05-18 11:22:25 +0000

    created checkout app and models

--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -13,7 +13,7 @@ def view_order(request):
 def add_to_order(request, item_id):
     """view for getting the quantity to add to the order"""
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('qty'))
     redirect_url = request.POST.get('redirect_url')
     order = request.session.get('order', {})
@@ -32,16 +32,16 @@ def add_to_order(request, item_id):
 def adjust_order(request, item_id):
     """view for adjusting the order"""
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     order = request.session.get('order', {})
     
     if item_id in list(order.keys()):
         order[item_id] = quantity
-        messages.info(request, f'adjusted {product.name} on your {order[item_id]}')
+        messages.info(request, f'adjusted {product.name} on your order to {order[item_id]} portions')
     else:
         order.pop(item_id)
-        messages.info(request, f'removed {product.name} from your order')
+        messages.info(request, f'adjusted {product.name} on your order to {order[item_id]} portions')
         
     request.session['order'] = order
     return redirect(reverse('view_order'))
@@ -51,12 +51,12 @@ def remove_from_order(request, item_id):
     """view for adjusting the order"""
     
     try:
-        product = Product.objects.get(pk=item_id)
+        product = get_object_or_404(Product, pk=item_id)
         order = request.session.get('order', {})
         
         if item_id in list(order.keys()):
             order[item_id] = 0
-            messages.info(request, f'adjusted {product.name} on your {order[item_id]}')
+            messages.info(request, f'removed {product.name} from your order!')
         else:
             order.pop(item_id)
             messages.info(request, f'removed {product.name} from your order')
@@ -64,4 +64,5 @@ def remove_from_order(request, item_id):
         request.session['order'] = order
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
