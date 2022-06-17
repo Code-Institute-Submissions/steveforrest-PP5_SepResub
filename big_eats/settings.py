87a73dabb5e31da95419f0f45549846ec048bab4
steveforrest <steveforrest_1@hotmail.com>
2022-06-17 09:34:28 +0000

    removed secret key and added to heroku

--- big_eats/settings.py
+++ big_eats/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vjx+m!kti+*l2@dve+=mha3=z5&no1d_@3jlk1t56zjt+4o+(y'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
