49b46c66aacfa55e4e01455d2f615432309eaa17
steveforrest <steveforrest_1@hotmail.com>
2022-06-20 11:38:42 +0000

    added aws settings to setting, created custom_storage file

--- big_eats/settings.py
+++ big_eats/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     
     #
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -188,6 +189,27 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'pp5-big-eatz'
+    AWS_S£_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS__ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
+    # Static and Media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+    
+    
+    # Overide static and media files in URLS in production
+    STATIC_URL = f'https://{AWS_S£_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S£_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
+
 
 #Stripe
 
