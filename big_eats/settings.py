2688df18ed1906ac55c57835b2378e006c6d2a53
steveforrest <steveforrest_1@hotmail.com>
2022-06-20 12:02:36 +0000

    added cache control1

--- big_eats/settings.py
+++ big_eats/settings.py
@@ -193,7 +193,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'EXPIRES': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
     
