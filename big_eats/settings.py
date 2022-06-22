a428cc3e1e9ae8026538e801cf20107c49d4580f
steveforrest <steveforrest_1@hotmail.com>
2022-06-22 09:29:01 +0000

    added outbound email settings1

--- big_eats/settings.py
+++ big_eats/settings.py
@@ -225,3 +225,11 @@ DEFAULT_CONTACT_NUMBER = '0121 564 5234'
 if 'DEVELOPEMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'bigeatz@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT =587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
