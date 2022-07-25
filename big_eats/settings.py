23f2ec9c8c804e9a3999d77dac9157d64661c1a9
steveforrest <steveforrest_1@hotmail.com>
2022-07-25 12:50:06 +0000

    changed settings to check issue with databases

--- big_eats/settings.py
+++ big_eats/settings.py
@@ -140,17 +140,17 @@ WSGI_APPLICATION = 'big_eats.wsgi.application'
 
 if development:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
-
+    }
+ 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
