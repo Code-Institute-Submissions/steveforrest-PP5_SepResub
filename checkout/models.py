33e389fd8b0349d7d471c99af984038e6efce037
steveforrest <steveforrest_1@hotmail.com>
2022-07-27 14:50:39 +0000

    adjusted review and invoice models

--- checkout/models.py
+++ checkout/models.py
@@ -17,7 +17,7 @@ class Invoice(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='orders')
+        related_name='invoice')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
